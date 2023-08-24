3e517938e28719665eb9b37f0666f566de97b3c2
diyoor <diyorsattarov7@gmail.com>
2023-08-24 03:38:24 -0500

    added extern/curl, test to JSONPlaceholder passes -- updated CMakeLists.txt

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -1,9 +1,47 @@
-#include <iostream>
 #include <gtest/gtest.h>
-#include <spdlog/spdlog.h>
+#include <curl/curl.h>
+#include <string>
 
-int main() {
-	spdlog::info("Welcome to spdlog!");
-	std::cout << "helloworld" << std::endl;
-	return 0;
+class JSONPlaceholderFixture : public ::testing::Test {
+protected:
+    CURL* curl;
+
+    JSONPlaceholderFixture() {
+        // Initialize cURL
+        curl_global_init(CURL_GLOBAL_ALL);
+        curl = curl_easy_init();
+    }
+
+    ~JSONPlaceholderFixture() {
+        // Cleanup cURL
+        if (curl) {
+            curl_easy_cleanup(curl);
+        }
+        curl_global_cleanup();
+    }
+};
+
+// Define a test case using the fixture
+TEST_F(JSONPlaceholderFixture, FetchPosts) {
+    if (!curl) {
+        GTEST_SKIP();
+    }
+
+    // Set API URL
+    std::string apiUrl = "https://jsonplaceholder.typicode.com/posts/1";
+
+    // Set up cURL request
+    curl_easy_setopt(curl, CURLOPT_URL, apiUrl.c_str());
+
+    // Perform the request
+    CURLcode res = curl_easy_perform(curl);
+
+    // Check if request was successful
+    ASSERT_EQ(res, CURLE_OK);
 }
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
