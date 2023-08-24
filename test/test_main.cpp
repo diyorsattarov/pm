eefbbd6aa59d90596d601dfd3fc6d8527b064fcc
diyoor <diyorsattarov7@gmail.com>
2023-08-24 01:20:34 -0500

    Added spdlog, gtest -- updated .gitignore for lib/ Makefile && removed / from /extern

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include <gtest/gtest.h>
+#include <spdlog/spdlog.h>
 
 int main() {
+	spdlog::info("Welcome to spdlog!");
 	std::cout << "helloworld" << std::endl;
 	return 0;
 }
